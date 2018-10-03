1228bd71fea022bbc0387ee363be1f0679518cc4
Timur Asayonok <t.asaynok@gmail.com>
2018-10-03 12:14:32 +0200

    Added error message prop for TextInput. Code is not for pull request.

--- src/Input.js
+++ src/Input.js
@@ -23,39 +23,52 @@ export default class DialogInput extends React.PureComponent {
       textInputRef,
       multiline,
       numberOfLines,
+      errorMessage,
+      error,
       ...otherProps
     } = this.props;
     const lines = (multiline && numberOfLines) || 1;
     const height = 18 + Platform.select({ ios: 14, android: 22 }) * lines;
     return (
-      <View style={[styles.textInputWrapper, wrapperStyle]}>
+      <View style={styles.container}>
         {label && <Text style={styles.label}>{label}</Text>}
-        <TextInput
-          ref={textInputRef}
-          style={[styles.textInput, style, { height }]}
-          multiline={multiline}
-          numberOfLines={numberOfLines}
-          {...otherProps}
-        />
+        <View style={[styles.textInputWrapper, wrapperStyle]}>
+          <TextInput
+            ref={textInputRef}
+            style={[styles.textInput, style, { height }]}
+            multiline={multiline}
+            numberOfLines={numberOfLines}
+            {...otherProps}
+          />
+        </View>
+        {error && <Text style={{ color: 'red', marginTop: 5, fontSize: 13 }}>{errorMessage}</Text>}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: Platform.select({
+    ios: {
+      marginHorizontal: 20,
+      marginBottom: 20
+    },
+    android: {
+      marginHorizontal: 10,
+      marginBottom: 20
+    }
+  }),
   textInputWrapper: Platform.select({
     ios: {
       backgroundColor: "white",
       borderWidth: StyleSheet.hairlineWidth,
       borderRadius: 6,
+      paddingHorizontal: 8,
       borderColor: "#A9ADAE",
-      marginHorizontal: 20,
-      marginBottom: 20,
-      paddingHorizontal: 8
     },
     android: {
-      marginHorizontal: 10,
-      marginBottom: 20
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      borderBottomColor: "#A9ADAE",
     }
   }),
   label: Platform.select({
@@ -73,3 +86,4 @@ const styles = StyleSheet.create({
     }
   })
 });
+
